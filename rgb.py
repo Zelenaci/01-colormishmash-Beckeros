a0d0a19dc142ac26d60c2517c70a548612429139
Beckeros <vychodil.albert@seznam.cz>
2022-01-03 13:42:00 +0100

    reee

--- rgb.py
+++ rgb.py
@@ -1,72 +1,119 @@
-#!/usr/bin/env python3
+
 
 from os.path import basename, splitext
 import tkinter as tk
+from tkinter import Label, Button, Scale, Canvas
 
+from tkinter import HORIZONTAL, LEFT, Frame, Entry, Canvas, S, StringVar, IntVar
 # from tkinter import ttk
 
 class Application(tk.Tk):
     name = basename(splitext(basename(__file__.capitalize()))[0])
-    name = "Foo"
+    name = "RGB"
 
     def __init__(self):
         super().__init__(className=self.name)
         self.title(self.name)
+        self.bind("<Escape>", self.quit)
 
-        self.bind("<Escape>", self.quit) #propojuje udalost s nejakou akci, zmackneme esc - program quitne
 
-        #R
-        self.frameR = tk.Frame()
-        self.frameR.pack()
-        self.lblR = tk.Label(self.frameR, text='R') #rika se tomu widgety
-        self.lblR.pack(side=tk.LEFT, anchor=tk.S) #umistime ho na screen
-        self.varR = tk.StringVar()
-        self.scaleR = tk.Scale(self.frameR, from_=0, to=255, orient=tk.HORIZONTAL, length=510, variable=self.varR, command=self.change) #from je keyword, tak se pise from_, aby to nebralo jako importovani
-        self.scaleR.pack(side=tk.LEFT, anchor=tk.S)
-        self.entryR = tk.Entry(self.frameR, width=5, textvariable=self.varR)
-        self.entryR.pack(side=tk.LEFT, anchor=tk.S)
 
-        #G
-        self.frameG = tk.Frame()
+
+#red
+        self.varR = IntVar()
+       
+        self.frameR= Frame(self)
+        self.frameR.pack()#umístí do hlavního okna
+        self.lblR = tk.Label(self.frameR, text="R")
+        self.lblR.pack(side=LEFT, anchor=S)
+        self.scaleR = Scale(self.frameR, from_=0, to=255, orient=HORIZONTAL, length=256,variable=self.varR)
+        self.scaleR.pack()
+
+        self.scaleR.pack(side=LEFT, anchor=S)
+        self.entryR = Entry(self.frameR, width=5, textvariable=self.varR)
+        self.entryR.pack(side=LEFT, anchor=S)
+
+
+
+
+
+
+#grn
+        self.varG = IntVar()
+       
+        self.frameG= Frame(self)
         self.frameG.pack()
-        self.lblG = tk.Label(self.frameG, text='G')
-        self.lblG.pack(side=tk.LEFT, anchor=tk.S)
-        self.varG = tk.StringVar()
-        self.scaleG = tk.Scale(self.frameG, from_=0, to=255, orient=tk.HORIZONTAL, length=510,variable=self.varG,  command=self.change) 
-        self.scaleG.pack(side=tk.LEFT, anchor=tk.S)
-        self.entryG = tk.Entry(self.frameG, width=5, textvariable=self.varG)
-        self.entryG.pack(side=tk.LEFT, anchor=tk.S)
+        self.lblG = tk.Label(self.frameG, text="G")
+        self.lblG.pack(side=LEFT, anchor=S)
+        self.scaleG = Scale(self.frameG, from_=0, to=255, orient=HORIZONTAL, length=256, variable=self.varG)
+        self.scaleG.pack()
 
-        #B
-        self.frameB = tk.Frame()
+        self.scaleG.pack(side=LEFT, anchor=S)
+        
+        self.entryG = Entry(self.frameG, width=5, textvariable=self.varG)
+        self.entryG.pack(side=LEFT, anchor=S)
+
+
+
+
+
+
+#blue
+
+
+        self.varB=IntVar()
+       
+        self.frameB= Frame(self)
         self.frameB.pack()
-        self.lblB = tk.Label(self.frameB, text='B')
-        self.lblB.pack(side=tk.LEFT, anchor=tk.S) 
-        self.varB = tk.StringVar()
-        self.scaleB = tk.Scale(self.frameB, from_=0, to=255, orient=tk.HORIZONTAL, length=510,variable=self.varB,  command=self.change)
-        self.scaleB.pack(side=tk.LEFT, anchor=tk.S)
-        self.entryB = tk.Entry(self.frameB, width=5, textvariable=self.varB)
-        self.entryB.pack(side=tk.LEFT, anchor=tk.S)
+        self.lblB = tk.Label(self.frameB, text="B")
+        self.lblB.pack(side=LEFT, anchor=S)
+        self.scaleB = Scale(self.frameB, from_=0, to=255, orient=HORIZONTAL, length=256, variable=self.varB)
+        self.scaleB.pack()
 
-        self.canvasMain = tk.Canvas(width=510, height=100, background='#000000')
+        self.scaleB.pack(side=LEFT, anchor=S)
+        self.entryB = Entry(self.frameB, width=5, textvariable=self.varB)
+        self.entryB.pack(side=LEFT, anchor=S)
+
+
+
+        self.canvasMain=Canvas(width=256, height=100, background="#000000")
         self.canvasMain.pack()
 
-        self.btn = tk.Button(self, text="Quit", command=self.quit) #widget
+        self.varMain=StringVar()
+        self.entryMain = Entry(self, textvariable=self.varMain, state="readonly", readonlybackground="#f3f3f3",)
+        self.entryMain.pack()
+
+
+        self.btn = tk.Button(self, text="Quit", command=self.quit)
         self.btn.pack()
 
-        self.btn2 = tk.Button(self, text="About", command=self.quit) #widget
+        self.btn2 = tk.Button(self, text="About", command=self.quit)
         self.btn2.pack()
 
-    def change(self, event):
-        
-        r = self.scaleR.get()
-        g = self.scaleG.get()
-        b = self.scaleB.get()
-        self.canvasMain.config(background=f'#{r:02x}{g:02x}{b:02x}')
 
-        self.varR.set(r)
-        self.varG.set(g)
-        self.varB.set(b)
+        self.varR.trace("w", self.change)
+        self.varG.trace("w", self.change)
+        self.varB.trace("w", self.change)
+
+
+
+
+
+
+
+
+    def change(self, var, index, mode):
+        #self.lblG.config(text="ahoj")
+
+        r = self.varR.get()
+        g = self.varG.get()
+        b = self.varB.get()
+
+        color=f"#{r:02x}{g:02x}{b:02x}"
+
+        self.canvasMain.config(background=color)#0 před 2 říká:vyplň prázdné místo nulami, 
+        self.varMain.set(color)
+      
 
 
     def quit(self, event=None):
